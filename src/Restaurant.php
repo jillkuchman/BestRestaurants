437d3dc0a334cf6c98bb1f6495ee1ca39ce5c067
nhu finney & Jill Kuchman <student@epicodus.com>
2015-03-18 12:12:56 -0700

    Add restaurant tests and methods

--- src/Restaurant.php
+++ src/Restaurant.php
@@ -13,16 +13,36 @@
             $this->cuisine_id = $cuisine_id;
         }
 
-        function setId($new_id)
-        {
-            $this->id = (int) $new_id;
-        }
-
         function getId()
         {
             return $this->id;
         }
 
+        static function getAll()
+        {
+            $returned_restaurants = $GLOBALS['DB']->query("SELECT * FROM restaurants;");
+            $restaurants = array();
+            foreach($returned_restaurants as $restaurant) {
+                $restaurant_name = $restaurant['restaurant_name'];
+                $id = $restaurant['id'];
+                $cuisine_id = $restaurant['cuisine_id'];
+                $new_restaurant = new Restaurant($restaurant_name, $id, $cuisine_id);
+                array_push($restaurants, $new_restaurant);
+            }
+            return $restaurants;
+        }
+
+        function save()
+        {
+            $statement = $GLOBALS['DB']->query("INSERT INTO restaurants (restaurant_name, cuisine_id) VALUES ('{$this->getRestaurantName()}', {$this->getCuisineId()}) RETURNING id;");
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            $this->setId($result['id']);
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM restaurants *;");
+        }
 
 
     }
--- tests/RestaurantTest.php
+++ tests/RestaurantTest.php
@@ -11,7 +11,99 @@
 
     class RestaurantTest extends PHPUnit_Framework_TestCase
     {
+        // protected function tearDown()
+        // {
+        //     Restaurant::deleteAll();
+        // }
 
+        function test_getId()
+        {
+            //Arrange
+            $food_type = "Pizza";
+            $id = null;
+            $test_cuisine = new Cuisine($food_type, $id);
+            $test_cuisine->save();
+
+            $restaurant_name = "Hot Lips Pizza";
+            $cuisine_id = $test_cuisine->getId();
+            $test_restaurant = new Restaurant($restaurant_name, $id, $cuisine_id);
+            $test_restaurant->save();
+
+            //Act
+            $result = $test_restaurant->getId();
+
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $food_type = "Pizza";
+            $id = null;
+            $test_cuisine = new Cuisine($food_type, $id);
+            $test_cuisine->save();
+
+            $restaurant_name = "Hot Lips";
+            $cuisine_id = $test_cuisine->getId();
+            $test_restaurant = new Restaurant($restaurant_name, $id, $cuisine_id);
+            $test_restaurant->save();
+
+            $restaurant2 = "Sizzle Pie";
+            $test_restaurant2 = new Restaurant($restaurant2, $id, $cuisine_id);
+            $test_restaurant2->save();
+
+            //Act
+            $result = Restaurant::getAll();
+
+            //Assert
+            $this->assertEquals([$test_restaurant, $test_restaurant2], $result);
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $food_type = "Italian";
+            $id = null;
+            $test_cuisine = new Cuisine($food_type, $id);
+            $test_cuisine->save();
+
+            $restaurant_name = "Mamamamas";
+            $cuisine_id = $test_cuisine->getId();
+            $test_restaurant = new Restaurant($restaurant_name, $id, $cuisine_id);
+
+            //Act
+            $test_restaurant->save();
+
+            //Assert
+            $result = Restaurant::getAll();
+            $this->assertEquals($test_restaurant, $result[0]);
+        }
+
+        // function test_deleteAll()
+        // {
+        //     //Arrange
+        //     $name = "Dog stuff";
+        //     $id = null;
+        //     $test_category = new Category($name, $id);
+        //     $test_category->save();
+        //
+        //     $description = "Wash the dog";
+        //     $category_id = $test_category->getId();
+        //     $test_task = new Task($description, $id, $category_id);
+        //     $test_task->save();
+        //
+        //     $description2 = "Water the dog";
+        //     $test_task2 = new Task($description2, $id, $category_id);
+        //     $test_task2->save();
+        //
+        //     //Act
+        //     Task::deleteAll();
+        //
+        //     //Assert
+        //     $result = Task::getAll();
+        //     $this->assertEquals([], $result);
+        // }
 
 
     }
