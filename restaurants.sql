d5b5ed36c8bc88c441727efa0c163c18dcde01cb
nhu finney & Jill Kuchman <student@epicodus.com>
2015-03-18 16:55:10 -0700

    Add restaurants.sql database file

---
+++ restaurants.sql
@@ -0,0 +1,187 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: cuisines; Type: TABLE; Schema: public; Owner: Guest; Tablespace: 
+--
+
+CREATE TABLE cuisines (
+    id integer NOT NULL,
+    food_type character varying
+);
+
+
+ALTER TABLE cuisines OWNER TO "Guest";
+
+--
+-- Name: cuisines_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE cuisines_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE cuisines_id_seq OWNER TO "Guest";
+
+--
+-- Name: cuisines_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE cuisines_id_seq OWNED BY cuisines.id;
+
+
+--
+-- Name: restaurants; Type: TABLE; Schema: public; Owner: Guest; Tablespace: 
+--
+
+CREATE TABLE restaurants (
+    id integer NOT NULL,
+    restaurant_name character varying,
+    cuisine_id integer
+);
+
+
+ALTER TABLE restaurants OWNER TO "Guest";
+
+--
+-- Name: restaurants_id_seq; Type: SEQUENCE; Schema: public; Owner: Guest
+--
+
+CREATE SEQUENCE restaurants_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE restaurants_id_seq OWNER TO "Guest";
+
+--
+-- Name: restaurants_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Guest
+--
+
+ALTER SEQUENCE restaurants_id_seq OWNED BY restaurants.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY cuisines ALTER COLUMN id SET DEFAULT nextval('cuisines_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Guest
+--
+
+ALTER TABLE ONLY restaurants ALTER COLUMN id SET DEFAULT nextval('restaurants_id_seq'::regclass);
+
+
+--
+-- Data for Name: cuisines; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY cuisines (id, food_type) FROM stdin;
+1	
+2	
+3	food
+4	food
+5	milk
+6	milk
+\.
+
+
+--
+-- Name: cuisines_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('cuisines_id_seq', 6, true);
+
+
+--
+-- Data for Name: restaurants; Type: TABLE DATA; Schema: public; Owner: Guest
+--
+
+COPY restaurants (id, restaurant_name, cuisine_id) FROM stdin;
+1	flower	5
+2	flower	5
+3	daisy	5
+4	daisy	5
+5	go	5
+6	go	5
+7	pizza	4
+8	cheese	4
+9	cheese	4
+\.
+
+
+--
+-- Name: restaurants_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
+--
+
+SELECT pg_catalog.setval('restaurants_id_seq', 9, true);
+
+
+--
+-- Name: cuisines_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace: 
+--
+
+ALTER TABLE ONLY cuisines
+    ADD CONSTRAINT cuisines_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: restaurants_pkey; Type: CONSTRAINT; Schema: public; Owner: Guest; Tablespace: 
+--
+
+ALTER TABLE ONLY restaurants
+    ADD CONSTRAINT restaurants_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: epicodus
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM epicodus;
+GRANT ALL ON SCHEMA public TO epicodus;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
