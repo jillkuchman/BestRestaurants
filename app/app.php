1c655aadd3c43b61cc70fec3d082667b689f7362
Jill Kuchman & nhu finney <student@epicodus.com>
2015-03-19 10:32:36 -0700

    Add edit page to cuisines

--- app/app.php
+++ app/app.php
@@ -7,6 +7,9 @@
     use Symfony\Component\Debug\Debug;
     Debug::enable();
 
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
     $app = new Silex\Application();
 
     $app['debug']=true;
@@ -42,7 +45,27 @@
         return $app['twig']->render('cuisines.twig', array('cuisine'=>$cuisine, 'restaurants'=>$cuisine->getRestaurants()));
     });
 
+    $app->post("/delete_cuisines", function() use ($app) {
+        Cuisine::deleteAll();
+        return $app['twig']->render('index.twig');
+    });
 
+    $app->post("/delete_restaurants", function() use ($app){
+        Restaurant::deleteAll();
+        return $app['twig']->render('index.twig');
+    });
+
+    $app->get("/cuisines/{id}/edit", function($id) use ($app){
+        $cuisine = Cuisine::find($id);
+        return $app['twig']->render('cuisine_edit.twig', array('cuisine' => $cuisine));
+    });
+
+    $app->patch("/cuisines/{id}", function($id) use ($app){
+        $food_type = $_POST['food_type'];
+        $cuisine = Cuisine::find($id);
+        $cuisine->updateType($food_type);
+        return $app['twig']->render('cuisines.twig', array('cuisine' => $cuisine, 'restaurants' => $cuisine->getRestaurants()));
+    });
 
     return $app;
 
